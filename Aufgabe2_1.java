3a218361df88c94f9fd9248622eaf91ffb9c64da
itsjulianbitch <38384975+itsjulianbitch@users.noreply.github.com>
2018-04-14 20:55:53 +0200

    Add files via upload

---
+++ Aufgabe2_1.java
@@ -0,0 +1,17 @@
+package Aufgaben;
+
+import java.util.Scanner;
+
+public class Aufgabe2_1 
+{
+
+	public static void main(String[ ] args) 
+	
+	{
+		System.out.print("Bitte geben Sie ihren Nachnamen ein:");
+		String name = new Scanner(System.in).nextLine();
+		
+		System.out.println("Guten Tag, Herr/Frau " + name + "!");
+	}
+
+}
